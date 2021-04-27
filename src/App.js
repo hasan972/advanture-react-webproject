bc0bba7c38a4cdc5dcef344ce2a78aace6b8fe6a
Shanto <rakibulhasansanto25@gmail.com>
2021-04-28 02:11:58 +0600

    Create other file

--- src/App.js
+++ src/App.js
@@ -1,16 +1,24 @@
 import React from 'react';
-import Navabr from './components/Navbar'
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import Navbar from './components/Navbar';
 import './App.css';
-import Home from './components/pages/Home'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Home from './components/pages/Home';
+
+import Products from './components/pages/Products';
+import SignUp from './components/pages/SignUp';
+import Services from './components/pages/Services';
 
 function App() {
   return (
     <>
       <Router>
-        <Navabr />
+        <Navbar />
         <Switch>
           <Route path='/' exact component={Home} />
+          <Route path='/services' component={Services} />
+
+          <Route path='/products' component={Products} />
+          <Route path='/sign-up' component={SignUp} />
         </Switch>
       </Router>
     </>
